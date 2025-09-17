3d6994a24f0203c1c9555007def1ac6bd6f93495
kuldeepguptahub <kuldeep.gupta2603@gmail.com>
2025-09-18 04:50:30 +0530

    updated frontend

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymongo
 from dotenv import load_dotenv
 import os
 
 # Setup MongoDB connection
 load_dotenv()
-mongo_uri = os.getenv("MONGO_URI")
+mongo_uri = os.getenv('MONGO_URL')
 client = pymongo.MongoClient(mongo_uri)
-db = client.get_database('mydatabase')
-collection = db.get_collection('mycollection')
+
+db = client.contactsdb
+collection = db.contacts
 
 # Initialize Flask app
 app = Flask(__name__)
+
 @app.route('/api')
 def home():
     with open('data.json') as file:
         data = file.read()
     return data
 
-@app.route('/', methods=['GET', 'POST'])
-def query_form():
-    return render_template('index.html')
+@app.route('/')
+def contact_form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    try:
+        form_data = dict(request.form)
+        collection.insert_one(form_data)
+        return render_template('submit.html')
+    except Exception as e:
+        error_message = f"Could not submit form: {str(e)}"
+        # re-render the same form with the error displayed
+        return render_template('form.html', error=error_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
